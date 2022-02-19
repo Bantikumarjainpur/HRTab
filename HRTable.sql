e35af6094ec4a19fb9ebdd29a7404850b5b9d719
Bantikumarjainpur <98106693+Bantikumarjainpur@users.noreply.github.com>
2022-02-19 12:09:33 +0530

    Add files via upload

---
+++ HRTable.sql
@@ -0,0 +1,21 @@
+select*from departments
+
+select max(salary) from employees
+
+select min(Salary) from employees
+
+select avg(Salary) from employees
+
+select first_name from employees
+
+select first_name,last_name from employees
+
+select department_id from employees
+
+select sum(salary) from employees
+
+select department_name from departments
+
+
+
+
